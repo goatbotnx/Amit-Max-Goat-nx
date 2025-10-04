8bd2c28e5172ecfbfcb06faa0df52cf4b8e1741a
fakereactionx210-hub <fakereactionx210@gmail.com>
2025-10-04 19:14:29 +0600

    Update gay.js

--- scripts/cmds/gay.js
+++ scripts/cmds/gay.js
@@ -1,116 +1,76 @@
-const DIG = require("discord-image-generation");
-const fs = require("fs-extra");
+const axios = require("axios");
+const fs = require("fs");
+const path = require("path");
 
 module.exports = {
-	config: {
-		name: "gay",
-		version: "1.0",
-		author: "@tas33n",
-		countDown: 1,
-		role: 0,
-		shortDescription: "find gay",
-		longDescription: "",
-		category: "box chat",
-		guide: "{pn} {{[on | off]}}",
-		envConfig: {
-			deltaNext: 5
-		}
-	},
+  config: {
+    name: "gay",
+    aliases: [],
+    version: "1.6",
+    author: "NeoKEX",
+    countDown: 2,
+    role: 0,
+    description: "Generate a gay image with two user IDs.",
+    category: "fun",
+    guide: {
+      en: "{pn} @mention @mention\nOr {pn} @mention\nOr reply to a message."
+    }
+  },
 
-	langs: {
-		vi: {
-			noTag: "Bạn phải tag người bạn muốn tát"
-		},
-		en: {
-			noTag: "You must tag the person you want to "
-		}
-	},
+  onStart: async function ({ api, event }) {
+    try {
+      const mentions = Object.keys(event.mentions || {});
+      let uid1, uid2;
+      let uid1Name, uid2Name;
 
-	onStart: async function ({ event, message, usersData, args, getLang }) 
-	{
+      // Case 1: Two or more mentions
+      if (mentions.length >= 2) {
+        uid1 = mentions[0];
+        uid2 = mentions[1];
+        uid1Name = event.mentions[uid1];
+        uid2Name = event.mentions[uid2];
+      }
+      // Case 2: One mention
+      else if (mentions.length === 1) {
+        uid1 = event.senderID;
+        uid2 = mentions[0];
+        const userInfo = await api.getUserInfo(uid1);
+        uid1Name = userInfo[uid1]?.name || "User";
+        uid2Name = event.mentions[uid2];
+      }
+      // Case 3: Reply to a message
+      else if (event.messageReply) {
+        uid1 = event.senderID;
+        uid2 = event.messageReply.senderID;
+        const userInfo = await api.getUserInfo([uid1, uid2]);
+        uid1Name = userInfo[uid1]?.name || "User";
+        uid2Name = userInfo[uid2]?.name || "User";
+      }
+      // Case 4: No mention or reply
+      else {
+        return api.sendMessage("Please reply to a message or mention one or two users.", event.threadID, event.messageID);
+      }
+      
+      const url = `https://neokex-apis.onrender.com/gay?uid1=${uid1}&uid2=${uid2}`;
+      const response = await axios.get(url, { responseType: 'arraybuffer' });
+      const filePath = path.join(__dirname, "cache", `gay_${uid1}_${uid2}.jpg`);
+      fs.writeFileSync(filePath, Buffer.from(response.data, "binary"));
 
-		let mention = Object.keys(event.mentions)
-		let uid;
+      const messageBody = `Oh yeah ${uid1Name} 💋 ${uid2Name}`;
+      const messageMentions = [
+        { tag: uid1Name, id: uid1 },
+        { tag: uid2Name, id: uid2 }
+      ];
 
-		// const img = await new DIG.Gay().getImage(url);
+      api.sendMessage({
+        body: messageBody,
+        attachment: fs.createReadStream(filePath),
+        mentions: messageMentions
+      }, event.threadID, () => fs.unlinkSync(filePath), event.messageID);
 
-
-		if(event.type == "message_reply"){
-		uid = event.messageReply.senderID
-		} else{
-			if (mention[0]){
-				uid = mention[0]
-			}else{
-				console.log(" jsjsj")
-				uid = event.senderID}
-		}
-
-let url = await usersData.getAvatarUrl(uid)
-let avt = await new DIG.Gay().getImage(url)
-
-
-	// 	message.reply({
-	// 		body:"",
-	// 		attachment: await global.utils.getStreamFromURL(avt)
-	// })
-			const pathSave = `${__dirname}/tmp/gay.png`;
-	fs.writeFileSync(pathSave, Buffer.from(avt));
-		let body = "look.... i found a gay"
-		if(!mention[0]) body="Baka you gay\nforgot to reply or mention someone"
-		message.reply({body:body,
-attachment: fs.createReadStream(pathSave)
-		}, () => fs.unlinkSync(pathSave));
-
-
-	}
+    } catch (e) {
+      console.error("Error:", e.message);
+      api.sendMessage("❌ Couldn't generate image. Try again later.", event.threadID, event.messageID);
+    }
+  }
 };
-
-
-
-
-
-
-
-
-
-// 	onStart: async function ({ message, event, usersData, threadsData, args }) {
-
-
-
-
-// 		if(event.type == "message_reply"){
-//       avt = await usersData.getAvatarUrl(event.messageReply.senderID)
-//     } else{
-//       if (!uid2){avt =  await usersData.getAvatarUrl(uid1)
-//               } else{avt = await usersData.getAvatarUrl(uid2)}}
-
-
-// 		message.reply({body:"Look.... I found a gay",
-// attachment: fs.createReadStream(pathSave)
-// 		}, () => fs.unlinkSync(pathSave));
-
-
-
-
-
-// message.send({body:"Look.... I found a gay",
-// attachment: fs.createReadStream(pathSave)
-// 		}, () => fs.unlinkSync(pathSave));
-
-// st fs = require("fs-extra");
-// let url = await usersData.getAvatarUrl(event.messageReply.senderID)
-// // const img = await new DIG.Gay().getImage(url);
-		// const pathSave = `${__dirname}/tmp/gay.png`;
-		// fs.writeFileSync(pathSave, Buffer.from(avt));
-
-// // message.send({body:"Look.... I found a gay",
-// // attachment: fs.createReadStream(pathSave)
-// // 		}, () => fs.unlinkSync(pathSave));
-
-
-// 	}
-
-
-
-
-// }
